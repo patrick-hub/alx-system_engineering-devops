2d29f795ba4c1699bc2178cf9d7ba466c3531f67
MAC <mac@MACs-MacBook-Pro.local>
2022-10-20 16:55:42 +0100

    web-server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,6 @@
-# Setup nginx server
+# Installs a Nginx server
 
-package { 'nginx':
-  ensure     => 'installed',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
-}
-
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt install nginx -y ; echo "Hello World!" | sudo tee /var/www/html/index.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/VicNW permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
